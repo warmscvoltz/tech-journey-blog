c349781cd7bb3acd024d2f3a888a3d2bbcafc97f
Luke Dunscombe <dunscombe1@gmail.com>
2023-07-20 16:43:26 +0000

    snorlax picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ type AllPostsData = {
 }[]
 
 // Change this url to change the image!
-const imageUrl = 'https://unsplash.com/photos/P_nM4HIeX8Y';
+const imageUrl = 'https://unsplash.com/photos/Zz4IIPPdoRU';
 const imageKey = imageUrl.split('/').at(-1);
 
 export default function Home() {

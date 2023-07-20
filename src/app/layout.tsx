d61145832a053be434a76b92ff18be6e34ce90df
Luke Dunscombe <dunscombe1@gmail.com>
2023-07-20 18:23:56 +0000

    Changing font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
+import {Source_Code_Pro} from 'next/font/google'
 import NavBarContainer from "@/components/NavBarContainer";
 import Footer from "@/components/Footer";
 
@@ -9,7 +9,7 @@ TODO:  Change these things along with:
   - avatar.jpeg in /public/images
   - favicon.ico in /public
  */
-const font = Inter({subsets: ['latin']})
+const font = Source_Code_Pro({ weight: 'variable', subsets: ['latin']})
 const title = 'Robert\'s Site';
 const description = 'This my personal site';
 const links = [
